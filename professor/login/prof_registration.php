3fee227af74b41110268e2d5adab67b4a84a2571
fatemeh.torkamani <fatemeh.torkamani.git@gmail.com>
2025-07-12 05:40:19 -0400

    prof register form changed

--- professor/login/prof_registration.php
+++ professor/login/prof_registration.php
@@ -39,26 +39,64 @@
             <input type="text" 
             class="input_box" placeholder="professor ID" name="prof_id">
             <br>
-            <input type="password" class="input_box" placeholder="password" name="pro_pass">
+            <input type="password" class="input_box" placeholder="password" name="prof_pass">
             <br>
             <input type="submit" class="input_box" value="Register" name="submit">
         </form>
     </div>
+  
     <style>
         h1{
             margin-bottom: 17px;
             
         }
+        
+        
     </style>
 </body>
 </html>
 
 
 <?php
+    if (isset($_POST["submit"])) {  
+      if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+        $first_name = filter_input(INPUT_POST,"first_name",FILTER_SANITIZE_SPECIAL_CHARS);
+        $last_name = filter_input(INPUT_POST,"last_name",FILTER_SANITIZE_SPECIAL_CHARS);
+        $username = filter_input(INPUT_POST,"username",FILTER_SANITIZE_SPECIAL_CHARS);
+        $prof_id = filter_input(INPUT_POST,"prof_id",FILTER_SANITIZE_SPECIAL_CHARS);
+        $prof_pass = filter_input(INPUT_POST,"prof_pass",FILTER_SANITIZE_SPECIAL_CHARS);
+        
+            
+            $req=['first_name','last_name','username','prof_id','prof_pass'];
+            $mis=[];
+
+            foreach ($req as $field) {
+                if(empty(trim($_POST[$field]))){
+                $mis [] = $field;
+                }
+               
+            }
+            if (!empty($mis)) {
+                $error_text = "please fill in : " . implode(', ',$mis);
+                echo "<script>alert('".addslashes($error_text)."');</script>";
+                }
+            else{
+                $hash_prof_pass = password_hash($prof_pass,PASSWORD_DEFAULT);
+
+                $Sql = "INSERT INTO professors (prof_id,prof_name,prof_family,prof_username,prof_pass) values
+                ('$prof_id','$first_name','$last_name','$username','$hash_prof_pass')";
+
+                mysqli_query($conn,$Sql);
+                echo "<script>alert('".addslashes("you registered successfully !! ")."');</script>";
+                }
+            
+        }
+
+     }
 
 
-
-
+  
 
 
 
