1c1dfe44b78ebcb9fb23dd0012353071469305a0
fatemeh.torkamani <fatemeh.torkamani.git@gmail.com>
2025-07-15 12:28:08 -0400

    openAI api added

---
+++ main/chatbot_api/chatbot.py
@@ -0,0 +1,56 @@
+# initial setup
+import openai
+from dotenv import find_dotenv, load_dotenv
+
+load_dotenv()
+
+client = openai.OpenAI()
+model = "gpt-4o"
+
+# # creating assistant
+
+# uni_web_ass = client.beta.assistants.create(
+#     name = "university website",
+#     instructions = """
+#              you are an ai assistant that helps university students with their problems and scientific questions and you have a good foundation in general knowledge and you know all languages and you can reply to students questions in different languages
+#       """,
+#     model=model
+# ) 
+
+
+
+# # creating thread
+
+# thread  = client.beta.threads.create(
+#     messages = [
+#         {
+#             "role":"user",
+#             "content":"what is your name"
+#         }
+#     ]
+# )
+# print(uni_web_ass.id)
+# print(thread.id)
+
+
+assis_id = "asst_9YKyOwSL2FgOdRN7fUJi0FPu"
+th_id = "thread_fPphp4wptcdk5rvI98Jso5Qs"
+
+
+# creating message/
+
+message = "how to focus on classes ?"
+message = client.beta.threads.messages.create(
+    thread_id=th_id,
+    role='user',
+    content=message
+)
+
+
+# running assis
+
+run = client.beta.threads.runs.create(
+    thread_id=th_id,
+    assistant_id=assis_id,
+    instructions='please address the user as a really intelligent person'
+)
