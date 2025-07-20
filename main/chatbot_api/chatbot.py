85f02f05ce016c4179867f832d846be97fec0d02
fatemeh.torkamani <fatemeh.torkamani.git@gmail.com>
2025-07-20 04:20:45 -0400

    main page frontend changed

--- main/chatbot_api/chatbot.py
+++ main/chatbot_api/chatbot.py
@@ -13,30 +13,30 @@ load_dotenv()
 client = openai.OpenAI()
 model = "gpt-4o"
 
-# # creating assistant
+# creating assistant
 
-# uni_web_ass = client.beta.assistants.create(
-#     name = "university website",
-#     instructions = """
-#              you are an ai assistant that helps university students with their problems and scientific questions and you have a good foundation in general knowledge and you know all languages and you can reply to students questions in different languages
-#       """,
-#     model=model
-# ) 
+uni_web_ass = client.beta.assistants.create(
+    name = "university website",
+    instructions = """
+             you are an ai assistant that helps university students with their problems and scientific questions and you have a good foundation in general knowledge and you know all languages and you can reply to students questions in different languages
+      """,
+    model=model
+) 
 
 
 
-# # creating thread
+# creating thread
 
-# thread  = client.beta.threads.create(
-#     messages = [
-#         {
-#             "role":"user",
-#             "content":"what is your name"
-#         }
-#     ]
-# )
-# print(uni_web_ass.id)
-# print(thread.id)
+thread  = client.threads.create(
+    messages = [
+        {
+            "role":"user",
+            "content":"what is your name"
+        }
+    ]
+)
+print(uni_web_ass.id)
+print(thread.id)
 
 
 assis_id = "asst_9YKyOwSL2FgOdRN7fUJi0FPu"
@@ -47,7 +47,7 @@ th_id = "thread_fPphp4wptcdk5rvI98Jso5Qs"
 # creating message/
 
 # message = "how to focus on classes ?"
-message = client.beta.threads.messages.create(
+message = client.threads.messages.create(
     thread_id=th_id,
     role="user",
     content="how to focus on classes ?"
@@ -56,7 +56,7 @@ message = client.beta.threads.messages.create(
 
 # running assis
 
-run = client.beta.threads.runs.create(
+run = client.threads.runs.create(
     thread_id=th_id,
     assistant_id=assis_id,
     instructions='please address the user as a really intelligent person'
@@ -82,10 +82,15 @@ def wait_for_run_completion(client,thread_id,run_id,sleep_interval=5):
         except Exception as e:
             logging.error(f"an error occured while retrieving the run : {e}")
             break
-
+        
         logging.info("waiting for run to complete ...")
         time.sleep(sleep_interval)    
             
 wait_for_run_completion(client=client,thread_id=th_id,run_id=run.id)            
+
+# ======steps --- logs =====
+run_steps = client.threads.runs.steps.list(run_id=run.id,thread_id=th_id)
+# print(run_steps.data[0])
+
 
       
